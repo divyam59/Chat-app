b8d65dc764986268a1a967d065f060d2fe66f7c0
divyam gupta <divyamg9905@gmail.com>
2021-05-25 09:30:14 +0530

    Message chat app, message on console only

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,15 +3,13 @@
 //return value of io will be store in a variable to access it
 //it will be used for both sending and receiving event from server or to client
 const socket = io()
-socket.on('countUpdated',(count)=>{
-    console.log('the count hase been updated and it is : '+count);
-})
-socket.on('message',(message)=>{
-console.log(message);
-})
 
-document.querySelector('#increment').addEventListener('click',()=>{
-    console.log('clicked');
-    //now we emit event from client and send it to server
-    socket.emit('increment')
+socket.on('message',(message)=>{
+    console.log(message);
+    })
+document.querySelector('#send').addEventListener('click',(e)=>{
+    //to prevent the automatic behaviour of button to refresh the page
+    e.preventDefault()
+    const message = document.querySelector('#mess').value;
+    socket.emit('sendMessage',message)
 })
--- src/index.js
+++ src/index.js
@@ -14,28 +14,16 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// a count variable
-let count = 0
+
 //socketio will be fired if socketiowill get a new connection
 //socket is object which consist infor connected client
 //we send event in socket.io i.e. to communicate with app and server, socket.io send events
 io.on('connection',(socket)=>{
     console.log('new Websocket connection')
-
-    //to send info from server to client
-    io.emit('countUpdated',count)
     io.emit('message','Welcome')
-
-    socket.on('increment',()=>{
-        count++;
-        //to emit changes to single client
-        // socket.emit('countUpdated',count)
-        //to emit the event to every single connection that's
-        //currently available and we are going to emit
-        io.emit('countUpdated',count)
-
+    socket.on('sendMessage',(message)=>{
+        io.emit('message',message)
     })
-
 })
 
 //2 syntax to print port name on console
